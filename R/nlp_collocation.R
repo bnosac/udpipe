610fe7f6a53567b09f616249b2968f70912b8456
Jan Wijffels <jwijffels@bnosac.be>
2017-11-12 22:18:43 +0100

    avoid r cmd check issues

--- R/nlp_collocation.R
+++ R/nlp_collocation.R
@@ -42,7 +42,8 @@
 #' @examples 
 #' data(brussels_reviews_anno)
 #' x <- subset(brussels_reviews_anno, language %in% "fr")
-#' colloc <- collocation(x, term = "lemma", group = c("doc_id", "sentence_id"), ngram_max = 3, n_min = 10)
+#' colloc <- collocation(x, term = "lemma", group = c("doc_id", "sentence_id"), 
+#'                       ngram_max = 3, n_min = 10)
 #' head(colloc, 10)
 #' 
 #' ## Example on finding collocations of nouns preceded by an adjective
@@ -52,7 +53,8 @@
 #' x[, xpos_next := txt_next(xpos, n = 1), by = list(doc_id, sentence_id)]
 #' x <- subset(x, (xpos %in% c("NN") & xpos_previous %in% c("JJ")) | 
 #'                (xpos %in% c("JJ") & xpos_next %in% c("NN")))
-#' colloc <- collocation(x, term = "lemma", group = c("doc_id", "sentence_id"), ngram_max = 2, n_min = 2)
+#' colloc <- collocation(x, term = "lemma", group = c("doc_id", "sentence_id"), 
+#'                       ngram_max = 2, n_min = 2)
 #' head(colloc)
 collocation <- function(x, term, group, ngram_max = 2, n_min = 2, sep = " "){
   ## R CMD check happiness
