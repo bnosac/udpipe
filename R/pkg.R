28ee431a471fd8210d4c53a3a76d9e1010fbdc50
Jan Wijffels <jwijffels@bnosac.be>
2018-01-03 14:44:38 +0100

    adding dtm_cbind and dtm_rbind

--- R/pkg.R
+++ R/pkg.R
@@ -1,6 +1,7 @@
 #' @importFrom Rcpp evalCpp
 #' @importFrom data.table tstrsplit ":=" data.table as.data.table setDF setDT setnames rbindlist setorder is.data.table frankv
 #' @importFrom utils download.file head
+#' @importFrom methods cbind2 rbind2
 #' @importFrom Matrix sparseMatrix summary crossprod tcrossprod diag 
 #' @useDynLib udpipe
 NULL
