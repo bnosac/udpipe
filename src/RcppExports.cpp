5639550b352282d3330765e7714f6597cbad1f3e
Jan Wijffels <jwijffels@bnosac.be>
2025-09-04 10:31:31 +0200

    no need for this newer version of Rcpp

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,11 +5,6 @@
 
 using namespace Rcpp;
 
-#ifdef RCPP_USE_GLOBAL_ROSTREAM
-Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
-Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
-#endif
-
 // dependency_rowlocations
 Rcpp::List dependency_rowlocations(const unsigned int& row, const Rcpp::List& x, const int depth);
 RcppExport SEXP _udpipe_dependency_rowlocations(SEXP rowSEXP, SEXP xSEXP, SEXP depthSEXP) {
